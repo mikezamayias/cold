fea142ed4fdbd19befc116113c6658ffaeeb04b4
Mike Zamayias <mzamagias@gmail.com>
2021-07-31 11:21:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cold
+
+A project that shows the current value of your crypto holdings.
---
+++ android/app/src/main/kotlin/com/example/cold/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cold
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
